b46e7dcde9e5f7d255f45a5737efa2ee5547de64
Noah Lee <noahlee@wirelessprv-10-193-206-7.near.illinois.edu>
2017-08-22 21:22:42 -0500

    simplified use of body-parser

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
-const multer = require('multer'); // v1.0.5
-var upload = multer(); // for parsing multipart/form-data
 
 const bots = require('./src/botmap');
 const handler = require('./src/main');
@@ -10,9 +8,7 @@ const handler = require('./src/main');
 // create Express app
 // about Express itself: https://expressjs.com/
 const app = express();
-
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 // setup view
 app.get('/', function(request, response) {
@@ -21,7 +17,7 @@ app.get('/', function(request, response) {
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-app.post('/:botName', upload.array(), (req, res) => {   // when bot webhook is called
+app.post('/:botName', (req, res) => {   // when bot webhook is called
     if (req.body.sender_type !== 'bot') {               // if message was not sent by a bot
         var bot = bots.get(botName);                    // get corresponding bot
         console.log('running ' + botName);
