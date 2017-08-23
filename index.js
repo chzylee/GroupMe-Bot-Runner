39a5f70ec3bc3c35cd076f56970296f1c94cc211
Noah Lee <noahlee@wirelessprv-10-193-206-7.near.illinois.edu>
2017-08-22 19:08:43 -0500

    log map

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const _bot = require('./src/main');
 
 // create LINE SDK config from env variables
 const config = {
-  botId: process.env.BOT_ID,
-  groupId: process.env.GROUP_ID,
-  accessToken: process.env.ACCESS_TOKEN
+    botId: process.env.BOT_ID,
+    groupId: process.env.GROUP_ID,
+    accessToken: process.env.ACCESS_TOKEN
 };
 
 // create Express app
@@ -22,27 +22,30 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 // setup view
 app.get('/', function(request, response) {
-  response.sendfile('./views/main.html');
+    response.sendfile('./views/main.html');
 });
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/webhook', upload.array(), (req, res) => {
   console.log('logging body');
-  console.log(req.body);
+  console.log(req.body.map(bot.messageHandler));
   res.json(req.body);
+    // Promise
+    //   .all(req.body.map(handleEvent))
+    //   .then((result) => res.json(result));
 });
 
 // event handler
 function handleEvent(message) {
-  // handle message
-  console.log(message);
-  var reply = bot.messageHandler(message, config);
-  Promise.resolve(reply);
+    // handle message
+    console.log(message);
+    var reply = bot.messageHandler(message, config);
+    Promise.resolve(reply);
 }
 
 // listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`listening on ${port}`);
+    console.log(`listening on ${port}`);
 });
