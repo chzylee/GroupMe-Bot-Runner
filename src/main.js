643c883052ba2a418efd3b8cb5285f1fc493fe77
Noah Lee <noahlee@wirelessprv-10-193-206-7.near.illinois.edu>
2017-08-22 21:34:57 -0500

    simplify logging

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 const utils = require('./utils');
 
 function handler(bot, message){
-    console.log('handling message:' + JSON.stringify(message))
+    console.log('handling message:' + message.text);
     var reply = bot.messageHandler(message);    // get reply from bot
     utils.respond(reply);                       // make request to API with reply
 }
