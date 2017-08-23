34f1ecf7c976496a66fca5a7f5f5520b4ff37240
Noah Lee <noahlee@wirelessprv-10-193-206-7.near.illinois.edu>
2017-08-22 21:27:52 -0500

    remove router

--- index.js
+++ index.js
@@ -8,18 +8,17 @@ const handler = require('./src/main');
 // create Express app
 // about Express itself: https://expressjs.com/
 const app = express();
-var router = new app.Router();
 
-router.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.json()); // for parsing application/json
 
 // setup view
-router.get('/', function(request, response) {
+app.get('/', function(request, response) {
     response.sendfile('./views/main.html');
 });
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-router.post('/:botName', (req, res) => {   // when bot webhook is called
+app.post('/:botName', (req, res) => {   // when bot webhook is called
     if (req.body.sender_type !== 'bot') {               // if message was not sent by a bot
         var bot = bots.get(botName);                    // get corresponding bot
         console.log('running ' + botName);
