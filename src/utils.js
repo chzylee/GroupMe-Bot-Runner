083b506793b35c17245334486f9bebefc1ac292d
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2017-09-17 23:25:45 -0500

    added random func and comments

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,7 @@ const request = require('request');
 
 module.exports = {
 
+    // checks if lowercase string includes one of options
     contains(lower, options) {
         for(var i = 0; i < options.length; i++) {        // for all options
             if(lower.includes(options[i])) {   // if lower contains one of options
@@ -11,6 +12,7 @@ module.exports = {
         return false; // if none of the options returned true
     },
 
+    // checks if lowercase string starts with one of options
     startsWith(lower, options) {
         for(var i = 0; i < options.length; i++) {        // for all options
             if(lower.startsWith(options[i])) {   // if lower starts with one of options
@@ -20,6 +22,13 @@ module.exports = {
         return false; // if none of the options returned true
     },
 
+    // returns random element from given array
+    randomFrom(arr){
+        var index = Math.ceil(Math.random() * (arr.length - 1));
+        return arr[index];
+    },
+
+    // forms json object of text reply message for HTML request 
     formTextReply(text, botId) {
         return {
             "bot_id": botId,
@@ -27,6 +36,7 @@ module.exports = {
         }
     },
 
+    // wraps http request to groupme api to send message back to group
     respond(message){
         var options = {
             uri: "https://api.groupme.com/v3/bots/post",
