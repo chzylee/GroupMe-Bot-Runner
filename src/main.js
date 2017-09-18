d8c7c4c311eb7dd7626403a0362d6fef4532fe71
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2017-09-17 23:18:49 -0500

    replies only if reply exists

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ const utils = require('./utils');
 function handler(bot, message){
     console.log('handling message:' + message.text);
     var reply = bot.messageHandler(message);    // get reply from bot
-    utils.respond(reply);                       // make request to API with reply
+    if(reply){
+        utils.respond(reply);                   // make request to API with reply
+    }
 }
 
 module.exports = handler;
