95d7828cf12308c670de6275209532e2bc685f0f
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2017-09-18 00:42:50 -0500

    reverted

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@
 const utils = require('./lib/utils');
 
 function handler(bot, message){
-    if(bot.status === 'off'){
-        console.log('bot is off');
-        return;
-    }
     console.log('handling message:' + message.text + 'with bot ' + bot.name);
     var reply = bot.messageHandler(message);    // get reply from bot given message object
     if(reply) {
