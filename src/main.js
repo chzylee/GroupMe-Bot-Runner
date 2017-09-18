94fc9a8beedc725e8a3c1ae6049de2125a4295d0
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2017-09-18 00:14:17 -0500

    detailed logging

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ const utils = require('./utils');
 function handler(bot, message){
     console.log('handling message:' + message.text);
     var reply = bot.messageHandler(message);    // get reply from bot given message object
-    if(reply){
+    if(reply) {
         utils.respond(reply);                   // make request to API with reply
+    }
+    else {
+        console.log('no reply');
     }
 }
 
