deb60c2c51d4f640b648b65cb088805e838741e9
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2017-09-18 00:06:13 -0500

    fixed comment

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const utils = require('./utils');
 
 function handler(bot, message){
     console.log('handling message:' + message.text);
-    var reply = bot.messageHandler(message);    // get reply from bot
+    var reply = bot.messageHandler(message);    // get reply from bot given message object
     if(reply){
         utils.respond(reply);                   // make request to API with reply
     }
