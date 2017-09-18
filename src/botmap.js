33335182925154f173dcce944962bafb61e2667d
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2017-09-17 23:19:04 -0500

    added punbot

--- src/botmap.js
+++ src/botmap.js
@@ -1,8 +1,10 @@
 'use strict'
 const _seed = require('./bots/seedbot');
+const _puns = require('./bots/punbot');
 
 // configure bot selection
 const bots = new Map();
 bots.set('seedbot', new _seed(process.env.SEED_ID));
+bots.set('punbot', new _puns(process.env.PUNS_ID))
 
 module.exports = bots;
