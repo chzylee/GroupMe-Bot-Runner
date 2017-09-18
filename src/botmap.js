0f61eb2621e8e01529ed9c49e8374f7de2cde9cc
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2017-09-18 00:50:34 -0500

    added 2nd punbot

--- src/botmap.js
+++ src/botmap.js
@@ -7,6 +7,7 @@ const _greetings = require('./bots/greetingbot');
 const bots = new Map();
 bots.set('seedbot', new _seed(process.env.SEED_ID));
 bots.set('punbot', new _puns(process.env.PUN_ID));
+bots.set('punbot2', new _puns(process.env.PUN2_ID));
 bots.set('greetingbot', new _greetings(process.env.GREET_ID));
 
 module.exports = bots;
